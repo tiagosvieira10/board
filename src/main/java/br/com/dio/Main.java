0bc1353aeab1425c4379547687b4ef20fe5388b8
Junior <junior.jr.jl@gmail.com>
2024-08-19 19:52:18 -0300

    feat: 2 criando o projeto

---
+++ src/main/java/br/com/dio/Main.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.persistence.migration.MigrationStrategy;
+
+import java.sql.SQLException;
+
+import static br.com.dio.persistence.config.ConnectionConfig.getConnection;
+
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+        try(var connection = getConnection()){
+            new MigrationStrategy(connection).executeMigration();
+        }
+    }
+
+}
