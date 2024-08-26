2e2d9257041a3e8ffb03d348a6d88f822eb99f7b
Junior <junior.jr.jl@gmail.com>
2024-08-26 19:59:35 -0300

    feat: 6 criando o menu de criação, exclusão e seleção de board

--- src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
+++ src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
@@ -6,7 +6,10 @@ import lombok.RequiredArgsConstructor;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import static br.com.dio.persistence.entity.BoardColumnKindEnum.findByName;
 
 @RequiredArgsConstructor
 public class BoardColumnDAO {
@@ -30,6 +33,21 @@ public class BoardColumnDAO {
     }
 
     public List<BoardColumnEntity> findByBoardId(final Long id) throws SQLException{
-        return null;
+        List<BoardColumnEntity> entities = new ArrayList<>();
+        var sql = "SELECT id, name, `order` FROM BOARDS_COLUMNS WHERE board_id = ? ORDER BY `order`";
+        try(var statement = connection.prepareStatement(sql)){
+            statement.setLong(1, id);
+            statement.executeQuery();
+            var resultSet = statement.getResultSet();
+            while (resultSet.next()){
+                var entity = new BoardColumnEntity();
+                entity.setId(resultSet.getLong("id"));
+                entity.setName(resultSet.getString("name"));
+                entity.setOrder(resultSet.getInt("order"));
+                entity.setKind(findByName(resultSet.getString("kind")));
+                entities.add(entity);
+            }
+            return entities;
+        }
     }
 }
