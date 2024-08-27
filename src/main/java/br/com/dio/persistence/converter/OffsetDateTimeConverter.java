3b9ba48f2c9eb6b7ee10f464926801bed946c9e1
Junior <junior.jr.jl@gmail.com>
2024-08-27 17:41:29 -0300

    feat: 10 criando a busca das informações do card

---
+++ src/main/java/br/com/dio/persistence/converter/OffsetDateTimeConverter.java
@@ -0,0 +1,18 @@
+package br.com.dio.persistence.converter;
+
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.time.OffsetDateTime;
+
+import static java.time.ZoneOffset.UTC;
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public final class OffsetDateTimeConverter {
+
+    public static OffsetDateTime toOffsetDateTime(final Timestamp value){
+        return OffsetDateTime.ofInstant(value.toInstant(), UTC);
+    }
+
+}
--- src/main/java/br/com/dio/persistence/dao/CardDAO.java
+++ src/main/java/br/com/dio/persistence/dao/CardDAO.java
@@ -1,17 +1,60 @@
 package br.com.dio.persistence.dao;
 
-import br.com.dio.dto.CardDetails;
+import br.com.dio.dto.CardDetailsDTO;
 import lombok.AllArgsConstructor;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import static br.com.dio.persistence.converter.OffsetDateTimeConverter.toOffsetDateTime;
 
 @AllArgsConstructor
 public class CardDAO {
 
     private Connection connection;
 
-    public CardDetails findById(final Long id){
-        return null;
+    public Optional<CardDetailsDTO> findById(final Long id) throws SQLException {
+        var sql =
+                """
+                SELECT c.id,
+                       c.title,
+                       c.description,
+                       b.blocked_at,
+                       b.block_reason,
+                       c.board_column_id,
+                       bc.name,
+                       COUNT(SELECT sub_b.id
+                               FROM BLOCKS sub_b
+                              WHERE sub_b.card_id = c.id) blocks_amount
+                  FROM CARDS c
+                  LEFT JOIN BLOCKS b
+                    ON c.id = b.card_id
+                   AND b.unblocked_at IS NULL
+                  INNER BOARDS_COLUMNS bc
+                     ON bc.id = c.board_column_id
+                  WHERE id = ?;
+                """;
+        try(var statement = connection.prepareStatement(sql)){
+            statement.setLong(1, id);
+            statement.executeQuery();
+            var resultSet = statement.getResultSet();
+            if (resultSet.next()){
+                var dto = new CardDetailsDTO(
+                        resultSet.getLong("c.id"),
+                        resultSet.getString("c.title"),
+                        resultSet.getString("c.description"),
+                        resultSet.getString("b.block_reason").isEmpty(),
+                        toOffsetDateTime(resultSet.getTimestamp("b.blocked_at")),
+                        resultSet.getString("b.block_reason"),
+                        resultSet.getInt("blocks_amount"),
+                        resultSet.getLong("c.board_column_id"),
+                        resultSet.getString("bc.name")
+                );
+                return Optional.of(dto);
+            }
+        }
+        return Optional.empty();
     }
 
 }
