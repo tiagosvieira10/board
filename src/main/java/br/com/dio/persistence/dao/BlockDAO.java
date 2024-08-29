0ed607aa46820b656a82566be5f6d300fb1da0a5
Junior <junior.jr.jl@gmail.com>
2024-08-29 11:12:06 -0300

    feat: 15 desbloqueio do card e conclusão do projeto

--- src/main/java/br/com/dio/persistence/dao/BlockDAO.java
+++ src/main/java/br/com/dio/persistence/dao/BlockDAO.java
@@ -24,4 +24,15 @@ public class BlockDAO {
         }
     }
 
+    public void unblock(final String reason, final Long cardId) throws SQLException{
+        var sql = "UPDATE BLOCKS SET unblocked_at = ?, unblock_reason = ? WHERE card_id = ? AND unblock_reason IS NULL;";
+        try(var statement = connection.prepareStatement(sql)){
+            var i = 1;
+            statement.setTimestamp(i ++, toTimestamp(OffsetDateTime.now()));
+            statement.setString(i ++, reason);
+            statement.setLong(i, cardId);
+            statement.executeUpdate();
+        }
+    }
+
 }
